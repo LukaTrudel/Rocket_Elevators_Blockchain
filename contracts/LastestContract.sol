e458a0bce4bb18d9d214c87eaa6dbfe460bd5dc3
YenXm <77247894+YenXm@users.noreply.github.com>
2021-12-23 15:39:45 -0500

    Create LastestContract.sol

---
+++ contracts/LastestContract.sol
@@ -0,0 +1,244 @@
+// SPDX-License-Identifier: GPL-3.0
+
+// Amended by HashLips
+/**
+    !Disclaimer!
+    These contracts have been used to create tutorials,
+    and was created for the purpose to teach people
+    how to create smart contracts on the blockchain.
+    please review this code on your own before using any of
+    the following code for production.
+    HashLips will not be liable in any way if for the use 
+    of the code. That being said, the code has been tested 
+    to the best of the developers' knowledge to work as intended.
+*/
+
+pragma solidity >=0.7.0 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+
+contract NFT is ERC721Enumerable, Ownable{
+  using Strings for uint256;
+  using SafeERC20 for IERC20;
+
+  string public baseURI;
+  string public baseExtension = ".json";
+  string public notRevealedUri;
+  uint256 public cost = 1000000 gwei;
+  uint256 public discountCost = 850000 gwei;
+  uint256 public maxSupply = 1000;
+  uint256 public maxMintAmount = 10;
+  uint256 public nftPerAddressLimit = 3;
+  uint256 public NFTRocketCost = 10;
+  uint256 public discountNFTRocketCost = 5;
+  bool public paused = false;
+  // bool public revealed = false;
+  bool public onlyWhitelisted = true;
+  uint public saleStart = 1640390399; //Date and time (GMT): Friday, December 24, 2021 11:59:59 PM
+  uint public saleEnd = 1640908799; //Date and time (GMT): Thursday, December 30, 2021 11:59:59 PM
+  // Our Client
+  address[] public whitelistedAddresses;
+  mapping(address => uint256) public addressMintedBalance;
+  address public rocketTokenAddress;
+
+
+
+  /// The sale has already been completed.
+  error SalesCompleted();
+
+  constructor(
+    string memory _name,
+    string memory _symbol,
+    string memory _initBaseURI,
+    string memory _initNotRevealedUri,
+    address _rocketToken
+  ) ERC721(_name, _symbol) {
+    setBaseURI(_initBaseURI);
+    setNotRevealedURI(_initNotRevealedUri);
+    setRocketTokenURI(_rocketToken);
+  }
+
+  // internal
+  function _baseURI() internal view virtual override returns (string memory) {
+    return baseURI;
+  }
+
+  function mintWithRocketTokens(uint _mintAmount) public {
+    require(!paused, "the contract is paused");
+    uint256 supply = totalSupply();
+    require(_mintAmount > 0, "need to mint at least 1 NFT");
+    require(_mintAmount <= maxMintAmount, "max mint amount per session exceeded");
+    require(supply + _mintAmount <= maxSupply, "max NFT limit exceeded");
+    IERC20 rocketToken = IERC20(rocketTokenAddress);
+    uint accountBalance = rocketToken.balanceOf(msg.sender);
+
+    if (msg.sender != owner()) {
+      // Correspond to presale
+      if (block.timestamp <= saleStart) {
+        // If address is whitelisted (is a client)
+        require (isWhitelisted(msg.sender), "address not whitelisted");
+        require (accountBalance >= discountNFTRocketCost * _mintAmount, "insufficient funds");
+        rocketToken.transferFrom(msg.sender, address(this), discountNFTRocketCost * _mintAmount);
+      // Correspond to official sale
+      } else if (block.timestamp <= saleEnd) {
+        require (accountBalance >= NFTRocketCost * _mintAmount, "insufficient funds");
+        rocketToken.transferFrom(msg.sender, address(this), NFTRocketCost * _mintAmount);
+      } else {
+        revert SalesCompleted();
+      }
+    }
+    
+    for (uint256 i = 1; i <= _mintAmount; i++) {
+      addressMintedBalance[msg.sender]++;
+      _safeMint(msg.sender, supply + i);
+    }
+  }
+
+  // public
+  function mint(uint256 _mintAmount) public payable {
+    require(!paused, "the contract is paused");
+    uint256 supply = totalSupply();
+    require(_mintAmount > 0, "need to mint at least 1 NFT");
+    require(_mintAmount <= maxMintAmount, "max mint amount per session exceeded");
+    require(supply + _mintAmount <= maxSupply, "max NFT limit exceeded");
+
+    if (msg.sender != owner()) {
+      // Correspond to presale
+      if (block.timestamp <= saleStart) {
+        // If address is whitelisted (is a client)
+        require (isWhitelisted(msg.sender), "address not whitelisted");
+        require (msg.value >= discountCost * _mintAmount, "insufficient funds");
+      // Correspond to official sale
+      } else if (block.timestamp <= saleEnd) {
+        require (msg.value >= cost * _mintAmount, "insufficient funds");
+      } else {
+        revert SalesCompleted();
+      }
+    }
+    
+    for (uint256 i = 1; i <= _mintAmount; i++) {
+        addressMintedBalance[msg.sender]++;
+      _safeMint(msg.sender, supply + i);
+    }
+  }
+  // public
+  function mintWithToken(uint256 _mintAmount) external payable {
+    require(!paused, "the contract is paused");
+    uint256 supply = totalSupply();
+    require(_mintAmount > 0, "need to mint at least 1 NFT");
+    require(_mintAmount <= maxMintAmount, "max mint amount per session exceeded");
+    require(supply + _mintAmount <= maxSupply, "max NFT limit exceeded");
+    require(block.timestamp <= saleEnd, "Sales Ended");
+
+    uint256 balance = IERC20(rocketTokenAddress).balanceOf(msg.sender);
+    require(balance >= _mintAmount, "You don't have enought token"); 
+
+    
+    for (uint256 i = 1; i <= _mintAmount; i++) {
+        addressMintedBalance[msg.sender]++;
+      _safeMint(msg.sender, supply + i);
+    }
+  }
+
+
+  
+  function isWhitelisted(address _user) public view returns (bool) {
+    for (uint i = 0; i < whitelistedAddresses.length; i++) {
+      if (whitelistedAddresses[i] == _user) {
+          return true;
+      }
+    }
+    return false;
+  }
+
+  function walletOfOwner(address _owner)
+    public
+    view
+    returns (uint256[] memory)
+  {
+    uint256 ownerTokenCount = balanceOf(_owner);
+    uint256[] memory tokenIds = new uint256[](ownerTokenCount);
+    for (uint256 i; i < ownerTokenCount; i++) {
+      tokenIds[i] = tokenOfOwnerByIndex(_owner, i);
+    }
+    return tokenIds;
+  }
+
+  function tokenURI(uint256 tokenId)
+    public
+    view
+    virtual
+    override
+    returns (string memory)
+  {
+    require(
+      _exists(tokenId),
+      "ERC721Metadata: URI query for nonexistent token"
+    );
+    
+    if(block.timestamp <= saleEnd) {
+        return notRevealedUri;
+    }
+
+    string memory currentBaseURI = _baseURI();
+    return bytes(currentBaseURI).length > 0
+        ? string(abi.encodePacked(currentBaseURI, tokenId.toString(), baseExtension))
+        : "";
+  }
+  function getCurrentCost() public view returns (uint) {
+    if (block.timestamp <= saleStart) {
+      return discountCost;
+    } else {
+      return cost;
+    }
+  }
+  
+  function setNftPerAddressLimit(uint256 _limit) public onlyOwner {
+    nftPerAddressLimit = _limit;
+  }
+  
+  function setCost(uint256 _newCost) public onlyOwner {
+    cost = _newCost;
+  }
+
+  function setmaxMintAmount(uint256 _newmaxMintAmount) public onlyOwner {
+    maxMintAmount = _newmaxMintAmount;
+  }
+
+  function setBaseURI(string memory _newBaseURI) public onlyOwner {
+    baseURI = _newBaseURI;
+  }
+
+  function setBaseExtension(string memory _newBaseExtension) public onlyOwner {
+    baseExtension = _newBaseExtension;
+  }
+  
+  function setNotRevealedURI(string memory _notRevealedURI) public onlyOwner {
+    notRevealedUri = _notRevealedURI;
+  }
+
+  function pause(bool _state) public onlyOwner {
+    paused = _state;
+  }
+  
+  function setOnlyWhitelisted(bool _state) public onlyOwner {
+    onlyWhitelisted = _state;
+  }
+  
+  function whitelistUsers(address[] calldata _users) public onlyOwner {
+    delete whitelistedAddresses;
+    whitelistedAddresses = _users;
+  }
+ 
+  function withdraw() public payable onlyOwner {
+    (bool os, ) = payable(owner()).call{value: address(this).balance}("");
+    require(os);
+  }
+  
+  function setRocketTokenURI(address _rocketToken) public onlyOwner {
+    rocketTokenAddress = _rocketToken;
+  }
+}
