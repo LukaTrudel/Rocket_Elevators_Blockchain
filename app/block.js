18a8569715e19d589b399841cdb4465a2c894ba0
YenXm <moonvoyager2@protonmail.com>
2021-12-23 21:36:37 -0500

    a

--- app/block.js
+++ app/block.js
@@ -1,4 +1,4 @@
-var contractAddressEthereumRinkeby = "0x4f7140284B91C4710d1B6e981cf17058F4D026B7";
+var contractAddressEthereumRinkeby = "0x1E6C60a4907AD2f002e58F08E4aCFE33ca07638E";
 var contractAddressPolygonMumbai = "0x3FD26641251096a799e8cB36284Fbeea944DEE69";
 var contractAbi = [
     {
